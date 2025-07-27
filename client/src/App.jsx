f03aec40ed836212510672a1c869d262ec575652
dspsilva27 <dspcdewnith@gmail.com>
2025-07-27 23:18:40 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import LoginPage from './pages/Loginpage';
 import SignUp from './pages/Signup';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+//import { useEffect, useState } from 'react';
+//import axios from 'axios';
 import InternDashboard from './pages/interndashboard';
+import SeatBookingPage from './pages/Seatbooking';
+import AddSeatsPage from './pages/Addseat';
+import AdminDashboard from './pages/admindashboard';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/interndashboard" element={<InternDashboard />} />
+      <Route path="/admindashboard" element={<AdminDashboard/>}/>
+      <Route path="/seatbooking" element={<SeatBookingPage/>}/>
+      <Route path="/addseats" element={<AddSeatsPage/>}/>
     </Routes>
   );
 }
